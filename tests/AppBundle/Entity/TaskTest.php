0f421bc9b5a1c3bae3d65c94ff6a961d0d643007
Bhalexx <minia-ture@hotmail.com>
2018-04-10 10:45:00 +0200

    Entities unit tests rewritten to test each method

--- tests/AppBundle/Entity/TaskTest.php
+++ tests/AppBundle/Entity/TaskTest.php
@@ -8,13 +8,36 @@ use Tests\AppBundle\SetupTest;
 
 class TaskTest extends SetupTest
 {
-    public function testTask()
+    private $validator;
+    private $user;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->validator = self::$kernel->getContainer()->get('validator');
+
+        $this->user = new User();
+        $this->user->setUsername('Leia');
+        $this->user->setPassword('password');
+        $this->user->setEmail('leia@mail.com');
+        $this->user->setRoles(['ROLE_USER']);
+    }
+
+    public function testConstructor()
+    {
+        $task = new Task();
+
+        $this->assertFalse($task->isDone());
+        $this->assertInstanceOf('DateTime', $task->getCreatedAt());
+    }
+
+    public function testId()
     {
         $user = $this->entityManager->getRepository('AppBundle:User')->findOneByUsername('darkdark');
 
         $task = new Task();
         $task->setTitle('Etoile Noire');
-        $task->setContent('Récupérer les plans volés par les rebelles');
+        $task->setContent('Récupérer les plans volés par les rebelles.');
         $task->setAuthor($user);
         $task->setCreatedAt(new \DateTime('2017-12-23 03:10:00'));
         $task->toggle(true);
@@ -23,12 +46,92 @@ class TaskTest extends SetupTest
         $this->entityManager->flush();
 
         $this->assertSame(10, $task->getId());
+    }
+
+    public function testTitle()
+    {
+        $task = new Task();
+        $this->assertNull($task->getTitle());
+
+        $task->setTitle('Etoile Noire');
         $this->assertSame('Etoile Noire', $task->getTitle());
-        $this->assertSame('Récupérer les plans volés par les rebelles', $task->getContent());
-        $this->assertSame($user, $task->getAuthor());
-        $this->assertTrue($task->isDone());
+    }
+
+    public function testTitleValidation()
+    {
+        $task = new Task();
+        $task->setTitle(null);
+        $task->setContent('Cacher les plans volés aux méchants dans R2.');
+        $task->setAuthor($this->user);
+
+        $errors = $this->validator->validate($task);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testContent()
+    {
+        $task = new Task();
+        $this->assertNull($task->getContent());
+
+        $task->setContent('Cacher les plans volés aux méchants dans R2.');
+        $this->assertSame('Cacher les plans volés aux méchants dans R2.', $task->getContent());
+    }
+
+    public function testContentValidation()
+    {
+        $task = new Task();
+        $task->setTitle('Etoile Noire');
+        $task->setContent(null);
+        $task->setAuthor($this->user);
+
+        $errors = $this->validator->validate($task);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testCreatedAt()
+    {
+        $task = new Task();
+
+        $task->setCreatedAt(new \DateTime('2017-12-23 03:10:00'));
         $this->assertEquals(new \DateTime('2017-12-23 03:10:00'), $task->getCreatedAt());
+    }
+
+    public function testToggle()
+    {
+        $task = new Task();
+        $this->assertFalse($task->isDone());
+
+        $task->toggle(true);
+        $this->assertTrue($task->isDone());
+    }
+
+    public function testAuthor()
+    {
+        $task = new Task();
+        $this->assertNull($task->getAuthor());
+
+        $task->setAuthor($this->user);
+        $this->assertSame($this->user, $task->getAuthor());
+    }
+
+    public function testIsAnonymous()
+    {
+        $task = new Task();
+        $user = new User();
+        $user->setUsername('Anonymous');
+
+        $task->setAuthor($user);
+        $this->assertTrue($task->isAnonymous());
+
+        $task->setAuthor($this->user);
         $this->assertFalse($task->isAnonymous());
-        $this->assertTrue($task->isAuthor($user));
+    }
+
+    public function testIsAuthor()
+    {
+        $task = new Task();
+
+        $task->setAuthor($this->user);
+        $this->assertTrue($task->isAuthor($this->user));
     }
 }
--- tests/AppBundle/Entity/UserTest.php
+++ tests/AppBundle/Entity/UserTest.php
@@ -2,13 +2,29 @@
 
 namespace Tests\AppBundle\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use AppBundle\Entity\Task;
 use AppBundle\Entity\User;
 use Tests\AppBundle\SetupTest;
 
 class UserTest extends SetupTest
 {
-    public function testUser()
+    private $validator;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->validator = self::$kernel->getContainer()->get('validator');
+    }
+
+    public function testConstructor()
+    {
+        $user = new User();
+
+        $this->assertInstanceOf(ArrayCollection::class, $user->getTasks());
+    }
+
+    public function testId()
     {
         $user = new User();
         $user->setUsername('Leia');
@@ -26,17 +42,98 @@ class UserTest extends SetupTest
         $this->entityManager->flush();
 
         $this->assertSame(5, $user->getId());
-        $this->assertSame('Leia', $user->getUsername());
-        $this->assertSame('leia@test.com', $user->getEmail());
-        $this->assertSame(['ROLE_USER'], $user->getRoles());
-        $this->assertNull($user->eraseCredentials());
     }
 
-    public function testUndefinedRoles()
+    public function testUsername()
+    {
+        $user = new User();
+
+        $this->assertSame(null, $user->getUsername());
+
+        $user->setUsername('Leia');
+        $this->assertSame('Leia', $user->getUsername());
+    }
+
+    public function testUsernameValidation()
+    {
+        $user = new User();
+        $user->setUsername(null);
+        $user->setEmail('leiaa@test.com');
+
+        $errors = $this->validator->validate($user);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testUsernameUnicityValidation()
+    {
+        $user = new User();
+        $user->setUsername('Leia');
+        $user->setEmail('leia@test.com');
+
+        $errors = $this->validator->validate($user);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testPassword()
+    {
+        $user = new User();
+
+        $this->assertSame(null, $user->getPassword());
+
+        $user->setPassword('password');
+        $this->assertSame('password', $user->getPassword());
+    }
+
+    public function testEmail()
+    {
+        $user = new User();
+
+        $this->assertSame(null, $user->getEmail());
+
+        $user->setEmail('leia@test.com');
+        $this->assertSame('leia@test.com', $user->getEmail());
+    }
+
+    public function testEmailValidation()
+    {
+        $user = new User();
+        $user->setUsername('Leia');
+        $user->setEmail(null);
+
+        $errors = $this->validator->validate($user);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testEmailUnicityValidation()
+    {
+        $user = new User();
+        $user->setUsername('Leia');
+        $user->setEmail('leia@test.com');
+
+        $errors = $this->validator->validate($user);
+        $this->assertEquals(1, count($errors));
+    }
+
+    public function testRoles()
     {
         $user = new User();
 
         $this->assertSame(['ROLE_USER'], $user->getRoles());
+
+        $user->setRoles(['ROLE_ADMIN']);
+        $this->assertSame(['ROLE_ADMIN'], $user->getRoles());
+    }
+
+    public function testEraseCredentials()
+    {
+        $user = new User();
+        $this->assertNull($user->eraseCredentials());
+    }
+
+    public function testSalt()
+    {
+        $user = new User();
+        $this->assertNull($user->getSalt());
     }
 
     public function testAddTask()
