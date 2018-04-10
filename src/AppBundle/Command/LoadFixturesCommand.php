520550d428945b5a6490ebf39c27f11b481c846c
Bhalexx <minia-ture@hotmail.com>
2018-04-10 18:20:04 +0200

    Code quality and travis yaml file

--- src/AppBundle/Command/LoadFixturesCommand.php
+++ src/AppBundle/Command/LoadFixturesCommand.php
@@ -14,14 +14,14 @@ use AppBundle\Entity\Task;
 
 class LoadFixturesCommand extends Command
 {
-    private $em;
+    private $entityManager;
     private $application;
 
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $entityManager)
     {
         parent::__construct();
 
-        $this->em = $em;
+        $this->entityManager = $entityManager;
     }
 
     protected function configure()
@@ -50,9 +50,9 @@ class LoadFixturesCommand extends Command
         $output->writeln('Create database');
         $this->createDatabase();
 
-        // Create database schema
-        $output->writeln('Create database schema');
-        $this->createDatabaseSchema();
+        // Create database schentityManagera
+        $output->writeln('Create database schentityManagera');
+        $this->createDatabaseSchentityManagera();
 
         //Load fixtures
         $this->loadFixtures();
@@ -79,11 +79,11 @@ class LoadFixturesCommand extends Command
     }
 
     /**
-     * Executes command to create database schema
+     * Executes command to create database schentityManagera
      */
-    private function createDatabaseSchema()
+    private function createDatabaseSchentityManagera()
     {
-        $options = array('command' => 'doctrine:schema:create');
+        $options = array('command' => 'doctrine:schentityManagera:create');
         $this->application->run(new ArrayInput($options));
     }
 
@@ -104,7 +104,7 @@ class LoadFixturesCommand extends Command
         //Load tasks
         $this->loadTasks($tasks);
 
-        $this->em->flush();
+        $this->entityManager->flush();
     }
 
     /**
@@ -120,9 +120,9 @@ class LoadFixturesCommand extends Command
             $user->setEmail($data['email']);
             $user->setRoles($data['roles']);
 
-            $this->em->persist($user);
+            $this->entityManager->persist($user);
         }
-        $this->em->flush();
+        $this->entityManager->flush();
     }
 
     /**
@@ -135,10 +135,10 @@ class LoadFixturesCommand extends Command
             $task = new Task();
             $task->setTitle($data['title']);
             $task->setContent($data['content']);
-            $task->setAuthor($this->em->getRepository('AppBundle:User')->findOneById($data['author']));
+            $task->setAuthor($this->entityManager->getRepository('AppBundle:User')->findOneById($data['author']));
             $task->toggle($data['isDone']);
 
-            $this->em->persist($task);
+            $this->entityManager->persist($task);
         }
     }
 }
